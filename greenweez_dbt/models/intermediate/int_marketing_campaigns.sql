3751af8fd24a94b106747168010ebcc4b2bfbf60
Helene Schweitzer <schweitzer.hel@gmail.com>
2026-04-30 00:17:43 +0200

    Install dbt_utils and use safe_divide in int_marketing_campaigns

--- greenweez_dbt/models/intermediate/int_marketing_campaigns.sql
+++ greenweez_dbt/models/intermediate/int_marketing_campaigns.sql
@@ -16,14 +16,8 @@ final AS (
         SUM(ads_cost)         AS ads_cost,
         SUM(ads_impressions)  AS ads_impressions,
         SUM(ads_clicks)       AS ads_clicks,
-        CASE WHEN SUM(ads_impressions) > 0
-             THEN SUM(ads_clicks) * 1.0 / SUM(ads_impressions)
-             ELSE 0
-        END                   AS click_through_rate,
-        CASE WHEN SUM(ads_clicks) > 0
-             THEN SUM(ads_cost) / SUM(ads_clicks)
-             ELSE 0
-        END                   AS cost_per_click
+        {{ dbt_utils.safe_divide('SUM(ads_clicks) * 1.0', 'SUM(ads_impressions)') }} as click_through_rate,
+        {{ dbt_utils.safe_divide('SUM(ads_cost)', 'SUM(ads_clicks)') }}              as cost_per_click
     FROM combined
     GROUP BY date_date, campaign_key, campaign_name, platform
 )
